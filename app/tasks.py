349dccca2badc5ae9673baf2e83c4b87f7433289
NIckB <schoolnick.ya@gmail.com>
2026-07-24 20:51:52 +0500

    refactoring in tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import logging
 
 import github
@@ -9,20 +10,20 @@ from sqlalchemy import select
 from telegram import LinkPreviewOptions
 from telegram.constants import ParseMode
 
-from app.github_obj import github_obj
-from app import telegram_bot
 from app.database import SessionLocal
 from app.database.models import Chat, Repo
 from app.database.models.chat_repo import ChatRepo
+from app.github_obj import github_obj
 from app.repo_engine import format_release_message, store_latest_release
+from app.telegram_bot import TelegramBot
 
 logger = logging.getLogger(__name__)
 
 
-def poll_github():
+def poll_github(telegram_bot: TelegramBot):
     with SessionLocal() as session:
         if not asyncio.run(telegram_bot.test_token()):
-            logger.critical('Telegram bot token is invalid or server not available')
+            logger.critical("Telegram bot token is invalid or server not available")
             return
 
         for repo_obj in session.scalars(select(Repo)):
@@ -33,15 +34,17 @@ def poll_github():
             try:
                 logger.info("Poll GitHub repo %s", repo_obj.full_name)
                 repo = github_obj.get_repo(repo_obj.id)
-            except github.UnknownObjectException as e:
+            except github.UnknownObjectException:
                 message = f"GitHub repo {repo_obj.full_name} has been deleted"
                 for chat in repo_obj.chats:
-                    try:
-                        asyncio.run(telegram_bot.send_message(chat_id=chat.id,
-                                                              text=message,
-                                                              disable_web_page_preview=True))
-                    except telegram.error.Forbidden as e:
-                        pass
+                    with contextlib.suppress(telegram.error.Forbidden):
+                        asyncio.run(
+                            telegram_bot.send_message(
+                                chat_id=chat.id,
+                                text=message,
+                                disable_web_page_preview=True,
+                            )
+                        )
 
                 logger.info(message)
                 session.delete(repo_obj)
@@ -51,33 +54,40 @@ def poll_github():
                 if e.status in (403, 451):
                     message = f"GitHub repo {repo_obj.full_name} has been blocked"
                     for chat in repo_obj.chats:
-                        try:
-                            asyncio.run(telegram_bot.send_message(chat_id=chat.id,
-                                                                  text=message,
-                                                                  disable_web_page_preview=True))
-                        except telegram.error.Forbidden as e:
-                            pass
+                        with contextlib.suppress(telegram.error.Forbidden):
+                            asyncio.run(
+                                telegram_bot.send_message(
+                                    chat_id=chat.id,
+                                    text=message,
+                                    disable_web_page_preview=True,
+                                )
+                            )
 
                     logger.info(message)
                     repo_obj.blocked = True
                     session.commit()
                 else:
-                    logger.error(f"GithubException for %s in poll_github: %s", repo_obj.full_name, e)
+                    logger.error(
+                        "GithubException for %s in poll_github: %s",
+                        repo_obj.full_name,
+                        e,
+                    )
                 continue
 
             if repo.archived and not repo_obj.archived:
                 message = f"GitHub repo <b>{repo_obj.full_name}</b> has been archived"
                 for chat in repo_obj.chats:
-                    try:
-                        asyncio.run(telegram_bot.send_message(chat_id=chat.id,
-                                                              text=message,
-                                                              parse_mode=ParseMode.HTML,
-                                                              link_preview_options=LinkPreviewOptions(
-                                                                  url=repo_obj.link,
-                                                                  prefer_small_media=True),
-                                                              ))
-                    except telegram.error.Forbidden as e:
-                        pass
+                    with contextlib.suppress(telegram.error.Forbidden):
+                        asyncio.run(
+                            telegram_bot.send_message(
+                                chat_id=chat.id,
+                                text=message,
+                                parse_mode=ParseMode.HTML,
+                                link_preview_options=LinkPreviewOptions(
+                                    url=repo_obj.link, prefer_small_media=True
+                                ),
+                            )
+                        )
 
                 logger.info(message)
                 repo_obj.archived = repo.archived
@@ -92,19 +102,24 @@ def poll_github():
                 logger.info("Process new release %s", release.name)
 
                 for chat in repo_obj.chats:
-                    message, parse_mode, entities = format_release_message(chat.release_note_format, repo, release)
+                    message, parse_mode, entities = format_release_message(
+                        chat.release_note_format, repo, release
+                    )
 
                     try:
-                        asyncio.run(telegram_bot.send_message(chat_id=chat.id,
-                                                              text=message,
-                                                              parse_mode=parse_mode,
-                                                              entities=entities,
-                                                              link_preview_options=LinkPreviewOptions(
-                                                                  url=repo_obj.link,
-                                                                  prefer_small_media=True),
-                                                              ))
-                    except telegram.error.Forbidden as e:
-                        logger.info('Bot was blocked by the user')
+                        asyncio.run(
+                            telegram_bot.send_message(
+                                chat_id=chat.id,
+                                text=message,
+                                parse_mode=parse_mode,
+                                entities=entities,
+                                link_preview_options=LinkPreviewOptions(
+                                    url=repo_obj.link, prefer_small_media=True
+                                ),
+                            )
+                        )
+                    except telegram.error.Forbidden:
+                        logger.info("Bot was blocked by the user")
                         session.delete(chat)
                         session.commit()
             elif isinstance(release_or_tag, Tag):
@@ -112,20 +127,25 @@ def poll_github():
                 logger.info("Process new tag %s", tag.name)
 
                 # TODO: Use tag.message as release_body text
-                message = (f"<a href='{repo.html_url}'>{repo.full_name}</a>:\n"
-                           f"<code>{tag.name}</code>")
+                message = (
+                    f"<a href='{repo.html_url}'>{repo.full_name}</a>:\n"
+                    f"<code>{tag.name}</code>"
+                )
 
                 for chat in repo_obj.chats:
                     try:
-                        asyncio.run(telegram_bot.send_message(chat_id=chat.id,
-                                                              text=message,
-                                                              parse_mode=ParseMode.HTML,
-                                                              link_preview_options=LinkPreviewOptions(
-                                                                  url=repo_obj.link,
-                                                                  prefer_small_media=True),
-                                                              ))
-                    except telegram.error.Forbidden as e:
-                        logger.info('Bot was blocked by the user')
+                        asyncio.run(
+                            telegram_bot.send_message(
+                                chat_id=chat.id,
+                                text=message,
+                                parse_mode=ParseMode.HTML,
+                                link_preview_options=LinkPreviewOptions(
+                                    url=repo_obj.link, prefer_small_media=True
+                                ),
+                            )
+                        )
+                    except telegram.error.Forbidden:
+                        logger.info("Bot was blocked by the user")
                         session.delete(chat)
                         session.commit()
             if isinstance(prerelease, GitRelease):
@@ -141,37 +161,46 @@ def poll_github():
                     if not chat_repo.process_pre_releases:
                         break
 
-                    message, parse_mode, entities = format_release_message(chat.release_note_format, repo, release)
+                    message, parse_mode, entities = format_release_message(
+                        chat.release_note_format, repo, release
+                    )
 
                     try:
-                        asyncio.run(telegram_bot.send_message(chat_id=chat.id,
-                                                              text=message,
-                                                              parse_mode=parse_mode,
-                                                              entities=entities,
-                                                              link_preview_options=LinkPreviewOptions(
-                                                                  url=repo_obj.link,
-                                                                  prefer_small_media=True),
-                                                              ))
-                    except telegram.error.Forbidden as e:
-                        logger.info('Bot was blocked by the user')
+                        asyncio.run(
+                            telegram_bot.send_message(
+                                chat_id=chat.id,
+                                text=message,
+                                parse_mode=parse_mode,
+                                entities=entities,
+                                link_preview_options=LinkPreviewOptions(
+                                    url=repo_obj.link, prefer_small_media=True
+                                ),
+                            )
+                        )
+                    except telegram.error.Forbidden:
+                        logger.info("Bot was blocked by the user")
                         session.delete(chat)
                         session.commit()
 
 
-def poll_github_user():
+def poll_github_user(telegram_bot: TelegramBot):
     with SessionLocal() as session:
         stmt = select(Chat).where(Chat.github_username.is_not(None))
         for chat in session.scalars(stmt):
             try:
                 github_user = github_obj.get_user(chat.github_username)
-            except github.GithubException as e:
-                logger.error(f"Can't found user '%s'", chat.github_username)
+            except github.GithubException:
+                logger.error("Can't found user '%s'", chat.github_username)
                 continue
 
             try:
-                asyncio.run(telegram_bot.add_starred_repos(chat.id, github_user, telegram_bot, session))
-            except telegram.error.Forbidden as e:
-                logger.info('Bot was blocked by the user')
+                asyncio.run(
+                    telegram_bot.add_starred_repos(
+                        chat.id, github_user, telegram_bot, session
+                    )
+                )
+            except telegram.error.Forbidden:
+                logger.info("Bot was blocked by the user")
                 session.delete(chat)
                 session.commit()
 
@@ -179,7 +208,7 @@ def poll_github_user():
                 try:
                     repo = github_obj.get_repo(repo_obj.id)
                 except github.GithubException as e:
-                    if e.status in (451,):
+                    if e.status == 451:
                         message = f"GitHub repo {repo_obj.full_name} has been blocked"
                         logger.info(message)
                     else:
